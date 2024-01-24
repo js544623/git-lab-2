2a48daac169f47e81dc1f18286fad90f53732ecb
js544623 <js544623@ohio.edu>
2024-01-23 19:47:40 -0500

    initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Olive Schoelkopf
+ *        Date: 1/23/24
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
